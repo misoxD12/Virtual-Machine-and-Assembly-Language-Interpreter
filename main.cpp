7ea86e76d35207bd971f337d1c1cc4eca2c332c5
MingSong <tohrucooldoragon@gmail.com>
2026-06-26 22:36:00 +0800

    most of the skeleton

--- main.cpp
+++ main.cpp
@@ -10,121 +10,228 @@ using namespace std;
 //data structure
 
 //registers
-class Register {
+class Register{
 protected:
-    int8_t value; //num store in this reg
+    signed char value; //num store in this reg
 public:
-    Register() { //constructor start reg at 0
+    Register(){ //constructor start reg at 0
         value = 0;
     }
 
-    int8_t getRegister() const { //get current value hold by the reg
+    virtual ~Register(){   
+    }
+
+    signed char getRegister() const{ //get current value hold by the reg
         return value;
     }
 
-    void setRegister(int8_t newValue) { //set new value for the reg
+    void setRegister(signed char newValue){ //set new value for the reg
         value = newValue;
     }
 };
 
 //represent r0 - r7
-class GeneralRegister : public Register {
+class GeneralRegister : public Register{
     int index;
 public:
-    GeneralRegister(int idx) { //constructor assign id  (0-7)
+    GeneralRegister(int idx){ //constructor assign id  (0-7)
         if (idx >= 0 && idx < 8){
             index = idx;
-        } else {
+        } else{
             cerr << "Invalid register number" << endl;
             index = -1;
         }
     }
 
-    int getIndex() { //get which register 
+    ~GeneralRegister(){   
+    }
+
+    int getIndex() const{ //get which register 
         return index;
     }
 };
 
 //4 Flags
-class FlagRegister {
+class FlagRegister{
         bool CarryFlag, ZeroFlag, UnderflowFlag, OverflowFlag;
 public:
     FlagRegister(){ //constructor init false
         CarryFlag = ZeroFlag = UnderflowFlag = OverflowFlag = false;
     }
+
+    ~FlagRegister(){ //destructor, nothing to clean up but added just in case
+    }
+
     //getter and setter for flags
-    bool getCarry() const {
+    bool getCarry() const{
         return CarryFlag;
     }
-    bool getZero() const {
+    bool getZero() const{
         return ZeroFlag;
     }
-    bool getUnderflow() const {
+    bool getUnderflow() const{
         return UnderflowFlag;
     }
-    bool getOverflow() const {
+    bool getOverflow() const{
         return OverflowFlag;
     }
 
-    void setCarry(bool value) {
+    void setCarry(bool value){
         CarryFlag = value;
     }
-    void setZero(bool value) {
+    void setZero(bool value){
         ZeroFlag = value;
     }
-    void setUnderflow(bool value) {
+    void setUnderflow(bool value){
         UnderflowFlag = value;
     }
-    void setOverflow(bool value) {
+    void setOverflow(bool value){
         OverflowFlag = value;
     }
-
-    void resetByName(string flagName) {
-        if (flagName == "CF") {
+    //reset flag by name
+    void resetByName(string flagName){
+        if (flagName == "CF"){
             CarryFlag = false;
-        } else if (flagName == "ZF") {
+        } else if (flagName == "ZF"){
             ZeroFlag = false;
-        } else if (flagName == "UF") {
+        } else if (flagName == "UF"){
             UnderflowFlag = false;
-        } else if (flagName == "OF") {
+        } else if (flagName == "OF"){
             OverflowFlag = false;
         } else {
             cerr << "Unknown flag name" << endl;
         }
     }
-
-    void updateFromResult(int result) {
-        if (result > 127) {
+    //update flag result
+    void updateFromResult(int result){
+        if (result > 127){
             OverflowFlag = true;
-        } else {
+        } else{
             OverflowFlag = false;
         }
-
-        if (result < -128) {
+        if (result < -128){
             UnderflowFlag = true;
-        } else {
+        } else{
             UnderflowFlag = false;
         }
-
-        if (result == 0) {
+        if (result == 0){
             ZeroFlag = true;
-        } else {
+        } else{
             ZeroFlag = false;
         }
-
-        if (result > 255 || result < 0) {
+        if (result > 255 || result < 0){
         CarryFlag = true;
-        } else {
+        } else{
         CarryFlag = false;
+        }
     }
 };
 
+//to be update
 int result = R[destination] + R[source];
 flags.updateFromResult(result);
 
+/** 
+int result = registers[destination]->get() + registers[source]->get();
+flags.updateFromResult(result);
+**/
+
+//operand
+//number these type
+enum OperandType {Register, Immediate, directMem, indirectMem};
+
+//to hold operand type and what value/register it involve
+class Operand{
+    OperandType type; //what type
+    int regIndex;     //which register if register/ indirect mem
+    int value;        //what value if immediate/ direct mem
+public: 
+    Operand(){ //constructor, default immediate with value 0
+        type = Immediate;
+        regIndex = -1;
+        value = 0;
+    }
+
+    //getters
+    OperandType getType() const{ //get operand type
+        return type;
+    }
+    int getRegIndex() const{
+        return regIndex;
+    }
+    int getValue() const{
+        return value;
+    }
+
+    //setters
+    void setType(OperandType t){
+        type = t;
+    }
+    void setRegIndex(int idx){
+        regIndex = idx;
+    }
+    void setValue(int v){
+        value = v;
+    }
+
+    bool isValidRegister(string text){ //checks if text looks like a valid register name like RNUM
+        if (text.length() == 2 && text [0] == 'R' && text [1] >= '0' && text [1] <= '7'){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    //read operand and figure which operand type it is
+    Operand readOperand(string text){
+        Operand op;
+        int len = text.length();
+        
+        //check got brackets?
+        bool hasBrackets = (text[0] == '[' && text[len - 1] == ']');
+
+        if (hasBrackets) {
+            //store the content in the brackets
+            string inside = "";
+            for (int i = 1; i < len-1; i++){
+                inside = inside + text [i];
+            }
+
+            if (isValidRegister(inside)){
+                op.setType(indirectMem);
+                int regNum = inside[1] - 1 - '0';
+                op.setRegIndex(regNum);
+            } else{
+                op.setType(directMem);
+                int address = 0;
+                for (int i = 0; i < (int)inside.length(); i++){
+                    address = address * 10 + (inside[i] - '0');
+                }
+                op.setValue(address);
+            }
+            return op;
+        }
+
+        if (isValidRegister(text)){
+            op.setType (Register);
+            int regNum = text[1] - '0';
+            op.setRegIndex(regNum);
+            return op;
+        }
+
+        op.setType(Immediate);
+        int num = 0;
+        for (int i = 0; i < len; i++){
+            num = num * 10 + (text[i] - '0');
+        }
+        op.setValue(num);
+        return op;
+    }
+};
+
+
 //memory
 
-//operand??
 
 //instructions
 
